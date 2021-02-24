864d82c2dec3add895d9eafe382a499c8152d1f1
xxx-NUMB <78338187+xxx-NUMB@users.noreply.github.com>
2021-02-24 12:10:47 +0800

    更新 pxt.json, main.blocks, PCA9554.ts

---
+++ PCA9554.ts
@@ -0,0 +1,42 @@
+
+//
+
+namespace pca9554 {
+    const PCA9554_ADDRESS  = 0x20
+    const PCA9554_REG_INP  = 0
+    const PCA9554_REG_OUT  = 1
+    const PCA9554_REG_POL  = 2
+    const PCA9554_REG_CTRL = 3
+
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf2 = pins.createBuffer(1)
+        buf2[0] = value
+        pins.i2cWriteBuffer(addr, buf2)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9554_ADDRESS, 0x00)
+        setFreq(500);
+        setPwm(0, 0, 4095);
+        for (let idx = 1; idx < 16; idx++) {
+            setPwm(idx, 0, 0);
+        }
+        initialized = true
+    }
+
+    function pca9554
+}
